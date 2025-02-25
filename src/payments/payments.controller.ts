9cdc15b0c383c8846521d036708dea773a3c6879
nguyeninnov8 <rn100204@gmail.com>
2025-02-25 14:46:06 +0700

    [ADD] pagination for GET request of payments

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -1,5 +1,5 @@
-import { ApiBearerAuth } from '@nestjs/swagger';
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiQuery } from '@nestjs/swagger';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 
 import { PaymentsService } from './payments.service';
 import { AdminGuard } from 'src/guards/admin.guard';
@@ -8,12 +8,17 @@ import { Response } from 'src/types/core';
 @ApiBearerAuth()
 @Controller('payments')
 export class PaymentsController {
-  constructor(private readonly paymentsService: PaymentsService) {}
+  constructor(private readonly paymentsService: PaymentsService) { }
 
   @UseGuards(AdminGuard)
   @Get()
-  async findAll(): Promise<Response> {
-    const data = await this.paymentsService.findAll();
-    return { data };
+  @ApiQuery({ name: 'page', required: false, type: Number, description: 'Page number' })
+  @ApiQuery({ name: 'limit', required: false, type: Number, description: 'Number of items per page' })
+  async findAll(
+    @Query('page') page: number = 1, 
+    @Query('limit') limit: number = 10
+  ): Promise<Response> {
+    const data = await this.paymentsService.findAll(page, limit);
+    return data;
   }
 }
