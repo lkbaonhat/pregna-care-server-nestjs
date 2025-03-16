85cc9cb93eed2579a726d7d898c1c9c0c2f39b51
baoha7204 <baoha3604@gmail.com>
2025-03-16 12:47:09 +0700

    [PCGMS02C05][BaoHMQ] define status for blog, remove some unused attributes

--- src/blog-post/blog-post.schema.ts
+++ src/blog-post/blog-post.schema.ts
@@ -1,41 +1,43 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
+import { BlogStatus } from './types/BlogStatus';
 
 export type BlogPostDocument = HydratedDocument<BlogPost>;
 
 @Schema({
-    timestamps: true,
+  timestamps: true,
 })
 export class BlogPost {
-    @Prop({ required: true })
-    author_id: string;
+  @Prop({ required: true })
+  author_id: string;
 
-    @Prop({ required: true })
-    heading: string;
+  @Prop({ required: true })
+  heading: string;
 
-    @Prop({ required: true })
-    page_title: string;
+  @Prop({ required: true })
+  content: string;
 
-    @Prop({ required: true })
-    content: string;
+  @Prop({ required: true })
+  description: string;
 
-    @Prop({ required: true })
-    description: string;
+  @Prop({ required: true })
+  feature_image_url: string;
 
-    @Prop({ required: true })
-    feature_image_url: string;
+  @Prop({ required: true })
+  published_date: string;
 
-    @Prop({ required: true })
-    url_handle: string;
+  @Prop({ required: true, default: true })
+  is_active: boolean;
 
-    @Prop({ required: true })
-    published_date: string;
+  @Prop({
+    required: true,
+    enum: Object.values(BlogStatus),
+    default: BlogStatus.Created,
+  })
+  status: BlogStatus;
 
-    @Prop({ required: true, default: true })
-    is_active: boolean;
-
-    @Prop({ required: true, default: 'published' })
-    status: string;
+  @Prop({ type: Number, default: -1 })
+  week?: number;
 }
 
 export const BlogPostSchema = SchemaFactory.createForClass(BlogPost);
---
+++ src/blog-post/types/BlogStatus.ts
@@ -0,0 +1,5 @@
+export enum BlogStatus {
+  Created = 'created',
+  Published = 'published',
+  Denied = 'denied',
+}
