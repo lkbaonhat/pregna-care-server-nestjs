a89cd086b8bd1e9ff780dcf0d15c81a6c59ddd84
baoha7204 <baoha3604@gmail.com>
2025-03-17 16:58:54 +0700

    [BaoHMQ] hotfix: remove heading, content in S3 upload single body

--- src/s3/s3.controller.ts
+++ src/s3/s3.controller.ts
@@ -1,80 +1,75 @@
 import {
-    Controller,
-    Post,
-    UploadedFile,
-    UploadedFiles,
-    UseInterceptors,
+  Controller,
+  Post,
+  UploadedFile,
+  UploadedFiles,
+  UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { S3Service } from './s3.service';
 import * as multer from 'multer';
 import { ApiConsumes, ApiBody, ApiTags, ApiResponse } from '@nestjs/swagger';
-import { Public } from 'src/constants/core';
-import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 import { Response } from 'src/types/core';
 
 @ApiTags('File Upload')
 @Controller('upload')
 export class S3Controller {
-    constructor(private readonly s3Service: S3Service) { }
+  constructor(private readonly s3Service: S3Service) {}
 
-    @Post('single')
-    @ApiConsumes('multipart/form-data')
-    @ApiBody({
-        schema: {
-            type: 'object',
-            properties: {
-                heading: {
-                    type: 'string',
-                },
-                content: {
-                    type: 'string',
-                },
-                file: {
-                    type: 'string',
-                    format: 'json',
-                },
-            },
+  @Post('single')
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'json',
         },
-    })
-    @ApiResponse({
-        status: 201,
-        description: 'File uploaded successfully',
-        schema: {
-            type: 'object',
-            properties: {
-                url: { type: 'string' },
-                key: { type: 'string' }
-            }
-        }
-    })
-    @UseInterceptors(FileInterceptor('file', { storage: multer.memoryStorage() }))
-    async uploadSingle(@UploadedFile() file: Express.Multer.File): Promise<Response> {
-        const result = await this.s3Service.uploadFile(file);
-        return {
-            data: result
-        };
+      },
+    },
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'File uploaded successfully',
+    schema: {
+      type: 'object',
+      properties: {
+        url: { type: 'string' },
+        key: { type: 'string' },
+      },
+    },
+  })
+  @UseInterceptors(FileInterceptor('file', { storage: multer.memoryStorage() }))
+  async uploadSingle(
+    @UploadedFile() file: Express.Multer.File,
+  ): Promise<Response> {
+    const result = await this.s3Service.uploadFile(file);
+    return {
+      data: result,
+    };
+  }
 
-    }
-
-    @Post('multiple')
-    @ApiConsumes('multipart/form-data')
-    @ApiBody({
-        schema: {
-            type: 'object',
-            properties: {
-                files: {
-                    type: 'array',
-                    items: {
-                        type: 'string',
-                        format: 'binary',
-                    },
-                },
-            },
+  @Post('multiple')
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        files: {
+          type: 'array',
+          items: {
+            type: 'string',
+            format: 'binary',
+          },
         },
-    })
-    @UseInterceptors(FilesInterceptor('files', 10, { storage: multer.memoryStorage() }))
-    async uploadMultiple(@UploadedFiles() files: Express.Multer.File[]) {
-        return this.s3Service.uploadMultipleFiles(files);
-    }
+      },
+    },
+  })
+  @UseInterceptors(
+    FilesInterceptor('files', 10, { storage: multer.memoryStorage() }),
+  )
+  async uploadMultiple(@UploadedFiles() files: Express.Multer.File[]) {
+    return this.s3Service.uploadMultipleFiles(files);
+  }
 }
